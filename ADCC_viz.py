019aa706b985b1001476f7ef48e554417bacc6d4
lizh1988 <88.li.zhihuan@gmail.com>
2023-10-01 22:39:11 +0800

    ADCC viz v2

--- ADCC_viz.py
+++ ADCC_viz.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import plotly.subplots as sp
 import plotly.graph_objects as go
 import streamlit as st
+import streamlit_elements as ste
 
 
 st.title("ADCC statistics")
@@ -50,28 +51,32 @@ st.header("An analysis of ADCC matches from 1998 to 2022", divider ='red')
 #Selecting gender
 st.select = st.selectbox('Gender', ['Male', 'Female'])
 
+chart1, chart2 = st.columns((2))
+
+with chart1:
+
 #Win type histogram
-if st.select=='Male':
-    hst=px.histogram(
-    dfm, y='weight_class', x='percentagecount', title = 'Distribution of win types across weight categories - Males',
-     barmode='stack', barnorm='percent', color ='win_type',
-    labels=dict(weight_class='Weight Class', percentagecount="occurence", win_type="Type of victory"),
-    category_orders={'Type of victory': ['POINTS', 'DECISION', 'SUBMISSION']}
-    )
-    hst.update_yaxes(categoryorder='array', categoryarray= ['66KG', '77KG', '88KG', '99KG', '+99KG', 'ABS'])
+    if st.select=='Male':
+        hst=px.histogram(
+        dfm, y='weight_class', x='percentagecount', title = 'Distribution of win types - Males',
+        barmode='stack', barnorm='percent', color ='win_type',
+        labels=dict(weight_class='Weight Class', percentagecount="occurence", win_type="Type of victory"),
+        category_orders={'Type of victory': ['POINTS', 'DECISION', 'SUBMISSION']}
+        )
+        hst.update_yaxes(categoryorder='array', categoryarray= ['66KG', '77KG', '88KG', '99KG', '+99KG', 'ABS'])
 
-elif st.select=='Female':
-    hst=px.histogram(
-    dff, y='weight_class', x='percentagecount', title = 'Distribution of win types across weight categories - Females', 
-    color='win_type', barmode='stack', barnorm='percent',
-    labels=dict(weight_class='Weight Class', percentagecount="occurence", win_type="Type of victory"),
-    category_orders={"Type of victory": ["Points", "Decision", "Submission"]}
-    )
-    hst.update_yaxes(categoryorder='array', categoryarray=['60KG', '+60KG'])
+    elif st.select=='Female':
+        hst=px.histogram(
+        dff, y='weight_class', x='percentagecount', title = 'Distribution of win types - Females', 
+        color='win_type', barmode='stack', barnorm='percent',
+        labels=dict(weight_class='Weight Class', percentagecount="occurence", win_type="Type of victory"),
+        category_orders={"Type of victory": ["Points", "Decision", "Submission"]}
+        )
+        hst.update_yaxes(categoryorder='array', categoryarray=['60KG', '+60KG'])
 
-hst.update_layout(xaxis_ticksuffix = '%', xaxis_title= 'Percentage within each weight class')
+    hst.update_layout(xaxis_ticksuffix = '%', xaxis_title= 'Percentage within each weight class')
 
-st.write(hst)
+    st.plotly_chart(hst,use_container_width=True)
 
 
 #Points difference histogram
@@ -81,26 +86,26 @@ dffpoints=dff[dff['win_type']=='POINTS']
 dfmpoints['points_diff']=(dfmpoints['winner_points']-dfmpoints['loser_points'])
 dffpoints['points_diff']=(dffpoints['winner_points']-dffpoints['loser_points'])
 
+with chart2:
+    if st.select=='Male':
+        hst1=px.histogram(
+        dfmpoints, x='points_diff', title = 'Distribution of points difference - Males',
+        labels=dict(points_diff='Difference in points'), color_discrete_sequence=['indianred']
+        )
 
-if st.select=='Male':
-    hst1=px.histogram(
-    dfmpoints, x='points_diff', title = 'Distribution of points difference in matches where athletes won by points - Males',
-    labels=dict(points_diff='Difference in points'), color_discrete_sequence=['indianred']
-    )
+    elif st.select=='Female':
+        hst1=px.histogram(
+        dffpoints, x='points_diff', title = 'Distribution of points difference - Females',
+        labels=dict(points_diff='Difference in points'), color_discrete_sequence=['indianred']
+        )
 
-elif st.select=='Female':
-    hst1=px.histogram(
-    dffpoints, x='points_diff', title = 'Distribution of points difference in matches where athletes won by points - Females',
-    labels=dict(points_diff='Difference in points'), color_discrete_sequence=['indianred']
-    )
+        hst1.update_traces(
+        xbins=dict(size=1),
+        )
+        hst1.update_xaxes(tickson='boundaries')
+    hst1.update_layout(xaxis=dict(tickmode = 'linear', tick0 = 0, dtick=1))
 
-    hst1.update_traces(
-    xbins=dict(size=1),
-    )
-    hst1.update_xaxes(tickson='boundaries')
-hst1.update_layout(xaxis=dict(tickmode = 'linear', tick0 = 0, dtick=1))
-
-st.write(hst1)
+    st.plotly_chart(hst1,use_container_width=True)
 
 dfmsub=dfm[dfm['submission'].notnull()]
 dfmsub['subcounts']=1
@@ -128,3 +133,5 @@ sb.update_traces(textinfo="label+percent parent")
 
 sb.update_layout(title='Breakdown of targets attacked and submission type by weight class', height=800)
 st.write(sb)
+
+
