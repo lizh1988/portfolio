6dc8884a5e8b0cf05a6ea96afb53ccec8364d8ca
lizh1988 <88.li.zhihuan@gmail.com>
2023-10-01 22:52:05 +0800

    1 Oct 2023

--- ADCC_viz.py
+++ ADCC_viz.py
@@ -104,7 +104,7 @@ with chart2:
         )
         hst1.update_xaxes(tickson='boundaries')
     hst1.update_layout(xaxis=dict(tickmode = 'linear', tick0 = 0, dtick=1))
-
+    hst1.update_traces(marker_line_width=1,marker_line_color="black")
     st.plotly_chart(hst1,use_container_width=True)
 
 dfmsub=dfm[dfm['submission'].notnull()]
